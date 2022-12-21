98c9dc7fa42239322ea7fbccd3c99ad6a8e0c6bb
Luciano Bitencourt <lucbite@gmail.com>
2022-12-21 06:08:11 -0300

    update
    up

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+// Calculo de IMC.
+/**var peso = parseFloat(prompt("Informe seu peso"));
+
+var altura = parseFloat(prompt("Informe sua altura"));
+
+
+imc = peso / (altura**2);
+
+console.log(imc.toFixed(2));
+
+document.write(`Seu imc esta ${imc.toFixed(2)}`);
+
+// Controle if-else
+
+if ( imc < 19) {
+    console.log("Abaixo do Peso");
+
+} else if (imc >= 19 && imc < 25) {
+    console.log(" Peso Normal");
+
+} else if (imc >= 25 && imc < 30)  {
+    console.log(" Sobrepeso");
+
+} else if (imc >= 30 && imc < 40)  {
+    console.log(" Obesidade - I");
+} else {
+    console.log(" Obesidade - II")
+}*/
+
+// ------------------------------
